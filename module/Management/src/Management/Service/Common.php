4f01c5c83c60698a84c9ce3574d362d65cb70a81
Abhinandan Kothari <abhinandan.kothari@synergytechservices.com>
2014-01-24 16:41:38 +0530

    Common Service
    Updated parent service class for new session changes.

--- module/Management/src/Management/Service/Common.php
+++ module/Management/src/Management/Service/Common.php
@@ -15,37 +15,7 @@ class Common {
 	}
 
 	public function getEntityManager(){
-		if(!$this->_em)
-			$this->_em = $this->getServiceLocator()->get('Doctrine\ORM\EntityManager');
-		return $this->_em;
+		if($this->_em)
+			return $this->_em;
 	}
-
-	protected function isLoggedIn(){
-		if (isset($this->_session->user->username)){
-			echo "A";
-		}if ($this->_session->afterLogout){
-			echo "B";
-		}if (isset($this->_session)){
-			echo "C";
-		}
-
-		if(isset($this->_session->user) || $this->_session->afterLogout || isset($this->_session)){
-			return true;
-		}else {
-			$this->redirectTo(array('controller'=>'login','action'=>'login'));
-		}
-
-		// 		if (isset($this->_session->user)){
-		// 			return true;
-		// 		}else {
-		// 			$this->redirectTo(array('controller'=>'login','action'=>'login'));
-		// 		}
-	}
-
-// 	private function isLoggedIn(){
-// 		if($this->_session->username)
-// 			return true;
-// 		else
-// 			$this->redirectTo(array('controller'=>'login','action'=>'login'));
-// 	}
 }
