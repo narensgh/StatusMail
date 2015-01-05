fde6e7146ce4620473d2d8851840ef7a9d02e9c2
Narendra Singh <narensgh@gmail.com>
2015-01-05 12:27:36 +0530

    added code for user change password and respected files

--- public/js/backbone/apps/router.js
+++ public/js/backbone/apps/router.js
@@ -1,36 +1,40 @@
 define(['jquery', 'backbone', 'apps/views/timesheet/signupView', 'apps/views/timesheet/userinfoView',
-    'apps/views/pm/templateView', 'apps/views/pm/projectView', 'apps/views/user/changepasswordView',
-    'apps/views/user/userprofileView'],
-        function($, Backbone, SignupView, UserinfoView, PmTemplateView, PmProjectView, UserchangepasswordView,
-                UserprofileView) {
-            var Router = Backbone.Router.extend({
-                routes: {
-                    "": "index",
-					"project": "project",
-                    "template": "template",
-                    'userinfo': 'userinfo',
-                    'changepassword': 'changepassword',
-                    'userprofile': 'userprofile'
-                },
-                el: $('body'),
-                index: function() {
-                    new SignupView();
-                },
-                userinfo: function(){
-                    new UserinfoView();
-                },
-				project: function() {
-					new PmProjectView();
-				},
-                template: function() {
-                    new PmTemplateView();
-                },
-                changepassword: function() {
-                    new UserchangepasswordView();
-                },
-                userprofile: function() {
-                    new UserprofileView();
-                }
-            });
-            return Router;
+    'apps/views/pm/templateView', 'apps/views/pm/projectView', 'apps/views/pm/todoListView',
+    'apps/views/user/changepasswordView', 'apps/views/user/userprofileView'],
+    function($, Backbone, SignupView, UserinfoView, PmTemplateView, PmProjectView, PmTodoListView, UserchangepasswordView,
+        UserprofileView) {
+        var Router = Backbone.Router.extend({
+            routes: {
+                "": "index",
+                "project": "project",
+                "project/todolist": "PmTodoList",
+                "template": "template",
+                'userinfo': 'userinfo',
+                'changepassword': 'changepassword',
+                'userprofile': 'userprofile'
+            },
+            el: $('body'),
+            index: function() {
+                new SignupView();
+            },
+            userinfo: function() {
+                new UserinfoView();
+            },
+            project: function() {
+                new PmProjectView();
+            },
+            template: function() {
+                new PmTemplateView();
+            },
+            changepassword: function() {
+                new UserchangepasswordView();
+            },
+            userprofile: function() {
+                new UserprofileView();
+            },
+            PmTodoList: function() {
+                new PmTodoListView();
+            }
         });
+        return Router;
+    });
